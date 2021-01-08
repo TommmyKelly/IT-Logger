dfc65cf912b10d16eea9f227fe5a21f3f4953071
TommmyKelly <tommy_kelly@icloud.com>
2021-01-08 11:28:02 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBnt from './components/layout/AddBnt';
+import AddLogModal from './components/logs/AddLogModal';
 
 const App = () => {
   useEffect(() => {
@@ -12,6 +15,11 @@ const App = () => {
   return (
     <Fragment>
       <SearchBar />
+      <div className="container">
+        <AddBnt />
+        <AddLogModal />
+        <Logs />
+      </div>
     </Fragment>
   );
 };
